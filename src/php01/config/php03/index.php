0ce4a01570367f2d5f8bd4b4fbb1b05a5185a36f
nakamura shunsuke <cagksa925@gmail.com>
2024-08-08 21:42:22 +0900

    リモートリポジトリの変更

---
+++ src/php01/config/php03/index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/common.css">
+    <linl rel="stylesheet"href="css/index.css">
+</head>
+<body>
+    <header class="header">
+     <div class="header_inner">
+      <a class="header_logo" href="/php03/index.php">
+        World Clock
+     </a>
+   </div>
+</header>
+
+</body>
+</html>
